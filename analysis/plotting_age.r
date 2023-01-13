ccc932dc75a0c558ac2f56377f5e892ff0be3a9a
v1kki213 <victoria.palin@manchester.ac.uk>
2023-01-13 13:49:22 +0000

    adding sex and plotting age dis

---
+++ analysis/plotting_age.r
@@ -0,0 +1,19 @@
+
+#read in study pop and create plot
+
+library('tidyverse')
+
+df_input <- read_csv(
+  here::here("output", "input.csv"),
+  col_types = cols(patient_id = col_integer(),age = col_double())
+)
+
+plot_age <- ggplot(data=df_input, aes(df_input$age)) + 
+    geom_histogram() +
+    labs(title = "Age Distribution",
+    x = "Age (years)")
+
+ggsave(
+  plot= plot_age,
+  filename="descriptive.png", path=here::here("output"),
+)
