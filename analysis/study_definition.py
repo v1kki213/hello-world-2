d5bb7403093316e183d5a165c8fc7e61bdba331c
v1kki213 <victoria.palin@manchester.ac.uk>
2023-01-13 13:15:42 +0000

    Update study_definition.py
    Added patient sex

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -8,14 +8,22 @@ study = StudyDefinition(
         "incidence": 0.5,
     },
     population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
+        "2019-01-01", "2023-01-01"
     ),
     
     age=patients.age_as_of(
-        "2019-09-01",
+        "2023-01-01",
         return_expectations={
             "rate": "universal",
             "int": {"distribution": "population_ages"},
         },
     ),
+
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.51}},
+        }
+    ),
+    
 )
