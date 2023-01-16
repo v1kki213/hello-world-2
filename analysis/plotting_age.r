3fa9adcb9cffa624c98564a865feea236c171f04
v1kki213 <victoria.palin@manchester.ac.uk>
2023-01-16 16:07:06 +0000

    adding region didnt work

--- analysis/plotting_age.r
+++ analysis/plotting_age.r
@@ -8,12 +8,23 @@ df_input <- read_csv(
   col_types = cols(patient_id = col_integer(),ageVar = col_double())
 )
 
-plot_age <- ggplot(data=df_input, aes(x=df_input$ageVar, fill=sex)) + 
-    geom_histogram() +
-    labs(title = "Age Distribution",
+plot_age <- ggplot(data=df_input, aes(x=df_input$ageVar, fill=sex)) +
+    geom_histogram() + 
+    labs(title = "Age Distribution", 
     x = "Age (years)")
 
 ggsave(
   plot= plot_age,
   filename="descriptive.png", path=here::here("output"),
 )
+
+#plot_age_region <- ggplot(data=df_input, aes(x=df_input$ageVar, fill=region)) +
+#    geom_histogram() + 
+#    stat_bin(binwidth=NULL)+
+#    labs(title = "Age Distribution", 
+#    x = "Age (years)")
+
+#ggsave(
+#  plot= plot_age_region,
+#  filename="descriptive_region.png", path=here::here("output"),
+#)
