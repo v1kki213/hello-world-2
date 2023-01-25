93f193a583566202494a1ba826ffa6a1971de215
v1kki213 <victoria.palin@manchester.ac.uk>
2023-01-25 14:49:27 +0000

    added ethnicity

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -44,6 +44,15 @@ study = StudyDefinition(
                 },
             },
          },
+    ),
+
+    ethnicity=patients.with_ethnicity_from_sus(
+    returning="group_6",
+    use_most_frequent_code=True,
+    return_expectations={
+            "category": {"ratios": {"1": 0.8, "5": 0.1, "3": 0.1}},
+            "incidence": 0.75,
+        },
     )
 )
     
